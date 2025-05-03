d6ac7a3382ea2e9a21247208a3f79876cb5ab477
김서연 <rainbow2870@naver.com>
2025-05-03 12:50:02 +0900

    Feat: ItemList 반응형 적용

--- src/components/Item/ItemList.jsx
+++ src/components/Item/ItemList.jsx
@@ -6,7 +6,7 @@ function ItemList({ items, className }) {
   return (
     <div className={className}>
       {items?.map((item) => (
-        <ItemCard key={item.id} item={item} />
+        <ItemCard key={item.id} item={item} className={className} />
       ))}
     </div>
   );
