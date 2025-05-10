51069b95e7f0eace942da76adc30e7bd08beb2a8
김서연 <rainbow2870@naver.com>
2025-05-10 10:29:43 +0900

    Refactor: Item Header 컴포넌트 두 개의 로고 이미지를 하나의 Link 태그 안으로 재배치

--- src/components/Item/Header.jsx
+++ src/components/Item/Header.jsx
@@ -16,14 +16,11 @@ function Header() {
     <header className="item-banner">
       <div className="item-banner-content">
         <div className="item-banner-left">
-          <div className="item-logo-content">
-            <Link className="item-logo" to="/">
-              <img src={logo} alt="판다마켓 로고 이미지" />
-            </Link>
-            <Link className="item-logo-text" to="/">
-              <img src={logo_title} alt="판다마켓" />
-            </Link>
-          </div>
+          <Link className="item-logo-content" to="/">
+            <img className="item-logo" src={logo} alt="판다마켓 로고 이미지" />
+            <img className="item-logo-text" src={logo_title} alt="판다마켓" />
+          </Link>
+
           <nav className="item-nav-content">
             <ul>
               <li>
